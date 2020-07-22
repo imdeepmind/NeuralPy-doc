1ed97d3c766256e40d26657d1cb029d4712a695a
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-07-23 01:01:48 +0530

    fixed some minor issues

--- src/components/mdxComponents/index.js
+++ src/components/mdxComponents/index.js
@@ -11,22 +11,22 @@ const StyledPre = styled('pre')`
 
 export default {
   h1: props => (
-    <h1 className="heading1" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
+    <h1 className="heading1" id={props.children && props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
   h2: props => (
-    <h2 className="heading2" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
+    <h2 className="heading2" id={props.children && props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
   h3: props => (
-    <h3 className="heading3" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
+    <h3 className="heading3" id={props.children && props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
   h4: props => (
-    <h4 className="heading4" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
+    <h4 className="heading4" id={props.children && props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
   h5: props => (
-    <h5 className="heading5" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
+    <h5 className="heading5" id={props.children && props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
   h6: props => (
-    <h6 className="heading6" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
+    <h6 className="heading6" id={props.children && props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
   p: props => <p className="paragraph" {...props} />,
   pre: props => (
